d011853ed6ab61afcd6a975cafdf87df59f2cc0b
Rintaro Kudo <rintaro.kudo@keio.jp>
2019-09-27 13:14:20 +0000

    validation update

--- models.rb
+++ models.rb
@@ -12,7 +12,7 @@ class User < ActiveRecord::Base
 has_secure_password
 validates :name,
 presence: true,
-format: { with: /.+@.+/ }
+format: { with: /\A[a-zA-Z0-9]+\z/ }
 validates :password,
 length: {in: 5..10}
 end
